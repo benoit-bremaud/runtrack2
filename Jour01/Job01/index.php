33074a68a4f9baae3f7eb3cd636d1fa9eb316ac0
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-05-22 13:20:45 +0200

    Update favicon
    7 - Mise à jour du chemin pour trouver favicon

--- Jour01/Job01/index.php
+++ Jour01/Job01/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/favicon.webp">
+    <link rel="icon" href="/images/favicon.webp">
     <title>PHP</title>
 </head>
 <body>
