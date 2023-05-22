0a3a4a425f83223bdb62e3711864994553a3ceeb
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-05-22 16:23:41 +0200

    First commit
    Création du fichier
    12 - affichage texte "hello laplateforme" + 1 commentaire en ligne
    13_15 - commentaire en block
    16 - retour à la ligne en console
    17 - affichage texte "hello laplateforme"

---
+++ Jour01/Job02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Titre principal</h1>
+    <?php
+        echo "Hello Laplateforme"; //voici un premier commentaire
+        /*
+        voici un deuxième commentaire
+        */
+        echo "<br>";
+        echo "Hello Laplateforme";
+    ?>
+</body>
+</html>
