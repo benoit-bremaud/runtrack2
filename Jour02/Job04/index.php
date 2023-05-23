ce4703298eed7a23c0dbce2bec92fe1ba798ec2c
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-05-23 15:00:34 +0200

    First commit
    Creation fichier

--- Jour02/Job04/index.php
+++ Jour02/Job04/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JOB3</title>
+    <title>JOB4</title>
     <link rel="icon" href="images/favicon.webp">
 </head>
 <body style="margin:0;">
@@ -16,7 +16,7 @@
     </nav>
     <main>
         <div style="background-color:grey; padding:5px 10px 5px 10px;">
-            <h2>Jour 2 - Job 03</h2>
+            <h2>Jour 2 - Job 04</h2>
             <h3>Affiche les nombres de 1 à 100 avec retour à la ligne :</h3>
             <h4>Puis remplacer certains nombres selon les conditions suivantes :</h4>
             <ul>
@@ -28,17 +28,13 @@
         <div style="background-color:purple; padding:5px 10px 5px 10px;">
         <h3>Début du programme :</h3>
         <?php
-            for ($i = 1; $i <= 100; $i++) { /*repète 100 fois une action*/
-                if ($i % 3 == 0 && $i % 5 == 0) {
-                    echo "FizzBuzz";
-                } elseif ($i % 3 == 0 && $i % 5 != 0) {
-                    echo "Fizz";
-                } elseif ($i % 3 != 0 && $i % 5 == 0) {
-                    echo "Buzz";
-                } else {
-                    echo "$i";
+            for ($i = 2; $i <= 1000; $i++) { /*repète 998 fois une action*/
+                for ($j = 2; $j <= $i; $j++) {
+                    if ($i % $j != 0) {
+                        echo "$i est un nombre premier<br>";
+                    }
                 }
-                echo "<br>";
+                // echo "<br>";
             }
         ?>
         </div>
