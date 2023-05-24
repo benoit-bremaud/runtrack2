4dad7771a9dca885a380a76fe094d6cca502da22
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-05-24 17:16:49 +0200

    build(not finished)

---
+++ Jour03/Job05/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="resources/images/favicon.webp">
+    <title>JOB5</title>
+</head>
+<body style="margin:0;">
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <div style="background-color:black; color:white;">
+            <h1>Runtrack2 - PHP</h1>
+                <p></p> 
+            <h2>Jour 03 - Job 05</h2>
+                <p></p>
+            <h3>Consignes :</h3>
+                <p>Créez une variable de type string nommée $str et affectez y le texte :
+    “On n’est pas le meilleur quand on le croit mais quand on le sait”.</p>
+    <p>Créez un dictionnaire (tableau keys/values) nommé $dic qui a comme keys
+    “consonnes” et “voyelles”.</p><p> Créez un algorithme qui parcourt, compte et stocke le
+    nombre d'occurrences de consonnes et de voyelles de $str.</p>
+    Affichez ces résultats dans un tableau &lt;table&gt; html qui a comme &lt;thead&gt; “Voyelles” et
+    “Consonnes”..</p>
+        </div>
+        <div>
+            <h3>Programme :</h3>
+                <?php
+                // Déclaration des variables
+                $str = "On n’est pas le meilleur quand on le croit mais quand on le sait.";
+                 
+                ?>
+
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
