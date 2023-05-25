08b39971e6270ae1275f4af9c0dadd0643c9f4b9
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-05-25 13:51:00 +0200

    build

--- Jour03/Job05/index.php
+++ Jour03/Job05/index.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+    <style>
+        table, th, td {
+            border:solid 1px black;
+        }
+        th, td {
+            width: 100px;
+            text-align: center;
+        }
+    </style>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,8 +41,47 @@
             <h3>Programme :</h3>
                 <?php
                 // Déclaration des variables
-                $str = "On n’est pas le meilleur quand on le croit mais quand on le sait.";
-                 
+                $str = "On n’est pas le meilleur quand on le croit mais quand on le sait";
+                $strlenght = strlen($str);
+                $dic = array('consonnes' => 0, 'voyelles' => 0 );
+                $voy = "aeiouy";
+                $voylenght = strlen($voy);
+                $cons = "bcdfghjklmnpqrstvwxz";
+                $conslenght = strlen($cons);
+
+                // début du programme
+                for ($i=0; $i < $strlenght; $i++) { 
+                    for ($j=0; $j < $conslenght; $j++) { 
+                        if (strtoupper($cons[$j]) == strtoupper($str[$i])) {
+                            $dic['consonnes']++;
+                        }
+                    }
+                    for ($k=0; $k < $voylenght; $k++) { 
+                        if (strtoupper($voy[$k]) == strtoupper($str[$i])) {
+                            $dic['voyelles']++;
+                        }
+                    }
+                }
+                ?>
+            <h3>Tableau valeurs :</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>consonnes</th>
+                            <th>voyelles</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $dic['consonnes']?></td>
+                            <td><?php echo $dic['voyelles']?></td>
+                        </tr>                
+                    </tbody>
+                </table>
+                <?php
+                // echo "$dic[consonnes]<br>";
+                // echo "$dic[voyelles]<br>";
+                // echo "array_keys($dizc, 0)"
                 ?>
 
         </div>
