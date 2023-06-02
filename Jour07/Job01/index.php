e363b5652de930928d2e93ff85943c8af7bad49d
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-02 16:25:30 +0200

    create(folders)

---
+++ Jour07/Job01/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./resources/images/favicon.webp" type="image/x-icon">
+    <link rel="stylesheet" href="./css/design.css">
+    <title>JOB01</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+                <svg viewBox="0 0 2500 350">
+                    <symbol id="s-text">
+                        <text text-anchor="middle" x="50%" y="70%">Runtrack2 - Session/Cookies</text>
+                    </symbol>
+
+                    <g class="g-ants">
+                        <use xlink:href="#s-text" class="text-copy"></use>
+                        <use xlink:href="#s-text" class="text-copy"></use>
+                        <use xlink:href="#s-text" class="text-copy"></use>
+                        <use xlink:href="#s-text" class="text-copy"></use>
+                        <use xlink:href="#s-text" class="text-copy"></use>
+                    </g>
+                </svg>
+            </div>
+        <nav>
+        </nav>
+    </header>
+    <main>
+        <h1></h1>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <h3></h3>
+        <p></p>
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
