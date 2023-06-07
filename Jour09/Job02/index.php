54b4db67eb3a7ce3bd300c29293a8c6755e8a34f
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-07 21:28:57 +0200

    Create job02.sql
    Création du programme
    Affiche Nom et Capacité des salles dans un tableau

---
+++ Jour09/Job02/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JOB02</title>
+</head>
+<body>
+<?php
+echo "<table style='border: solid 1px black;'>"; # Définition des contours du tableau
+echo "<tr><th>Nom</th><th>Capacité</th></tr>"; # affichage des titres du tableau
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:auto;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jour09";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT nom, capacite FROM salles");
+  $stmt->execute();
+  
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {  echo 'Erreur :' . $e->getMessage();  }
+
+$conn = null;
+
+echo '</table>';
+
+?>
+</body>
+</html>
